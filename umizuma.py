941facf21905d5c596ec685d27e6b942de63554a
Johan Park <johanpark@Johans-Air.home>
2023-01-12 22:56:32 -1000

    first commit with game file

---
+++ umizuma.py
@@ -0,0 +1,8 @@
+"""
+all players throw out a single hand, each showing zero to five fingers, and call out their guess at what the sum of all fingers shown will be. If one player guesses the sum, that player earns one point. The first player to reach three points wins the game.
+"""
+
+class Umizuma:
+    """ Defines all functions needed to play a game of umizuma """
+
+    
